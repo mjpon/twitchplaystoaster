1b9a0c7178d71f05b8d5e277b3b81b3a5a7a49f8
deehrlic <47901184+deehrlic@users.noreply.github.com>
2020-01-18 19:29:35 -0800

    it turns!

--- motors/motors.py
+++ motors/motors.py
@@ -5,32 +5,76 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
+GPIO.setup(11,GPIO.OUT)
+p = GPIO.PWM(11,50)
+p.start(0)
+SERVO_SLEEP_TIME = 3
+WHEEL_SLEEP_TIME = 3
 
-def spinMotor(pin):
-	GPIO.setup(pin,GPIO.OUT)
-	GPIO.output(pin,GPIO.HIGH)
-	time.sleep(3)
-	GPIO.output(pin,GPIO.LOW)
+def spinMotor(pin, dir):
+	print(dir)
+
+	if dir == "left":
+		#SERVO MOTOR
+		p.ChangeDutyCycle(7.5)
+		#time.sleep(1)
+		p.ChangeDutyCycle(6.5)
+		time.sleep(SERVO_SLEEP_TIME)
+		p.ChangeDutyCycle(7.5)
+		#time.sleep(1)
+	elif dir == "right":
+		p.ChangeDutyCycle(7.5)
+		#time.sleep(1)
+		p.ChangeDutyCycle(8.5)
+		time.sleep(SERVO_SLEEP_TIME)
+		p.ChangeDutyCycle(7.5)
+		#time.sleep(1)
+	elif dir == "forward":
+		GPIO.setup(pin,GPIO.OUT)
+		GPIO.output(pin,GPIO.HIGH)
+		time.sleep(WHEEL_SLEEP_TIME)
+		GPIO.output(pin,GPIO.LOW)
+	else:
+		print("ERROR ERROR")
 
 if __name__ == "__main__":
 
-	left = 11
+	left = 13
 	right = 16
 
 	args = str(sys.argv[1])
 	print(args)
 
 	if args == "forward":
-		t1 = threading.Thread(target=spinMotor, args=(11,))
-		t2 = threading.Thread(target=spinMotor, args=(16,))
+		dir = "forward"
+		t1 = threading.Thread(target=spinMotor, args=(13,"forward",))
+		t2 = threading.Thread(target=spinMotor, args=(16,"forward",))
 		t1.start()
 		t2.start()
 		t1.join()
 		t2.join()
 	elif args == "left":
-		spinMotor(left)
+		dir = "left"
+		t0 = threading.Thread(target=spinMotor, args=(13,"left",))
+		t1 = threading.Thread(target=spinMotor, args=(13,"forward",))
+		t2 = threading.Thread(target=spinMotor, args=(16,"forward",))
+		t0.start()
+		t1.start()
+		t2.start()
+		t0.join()
+		t1.join()
+		t2.join()
 	elif args == "right":
-		spinMotor(right)
+		dir = "right"
+		t0 = threading.Thread(target=spinMotor, args=(13,"right",))
+		t1 = threading.Thread(target=spinMotor, args=(13,"forward",))
+		t2 = threading.Thread(target=spinMotor, args=(16,"forward",))
+		t0.start()
+		t1.start()
+		t2.start()
+		t0.join()
+		t1.join()
+		t2.join()
 
 	##TODOS
 	##ADD SPECIAL
--- motors/mqtt_pi.py
+++ motors/mqtt_pi.py
@@ -1,4 +1,10 @@
 import paho.mqtt.client as mqtt
+import os
+
+#IP OF THE DEVICE RUNNNING THIS CODE GOES HERE, CHANGE IF USING
+#MQTT_SERVER = "67.180.229.63"
+MQTT_SERVER = "10.0.0.131"
+MQTT_PATH = "test"
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
@@ -6,7 +12,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("$SYS/#")
+    client.subscribe(MQTT_PATH)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -15,14 +21,15 @@ def on_message(client, userdata, msg):
     #if str(msg.payload) == str(b'opendoor'):
     #	os.system("python3 motor.py")
 
+# more callbacks, etc
+
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("10.0.0.131", 1883, 60)
-#client.publish("WEE WOO WEE WOO")
+client.connect(MQTT_SERVER, 1883, 60)
+
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-client.loop_forever()
